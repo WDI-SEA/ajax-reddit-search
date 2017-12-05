35030c31bce8ebd69ad823cdbb81bb7e00d409e5
brandiw <brandimichelle.0911@gmail.com>
2017-12-05 09:48:31 -0800

    Distinguishing solution branch

--- ajax.js
+++ ajax.js
@@ -1,7 +1,7 @@
 // Set up the page when it loads.
 $(function() {
   // attach the form submission to the search function
-  $("#search-form").on('submit', search);
+  //TO DO
 });
 
 function search(event) {
@@ -12,42 +12,22 @@ function search(event) {
 
   // Get the users search input and save it in a variable.
   // Use the input placeholder value (like "kittens") as a default value.
-  var input = $("#query");
-  var userQuery = input.val() || input.attr("placeholder");
-
-  console.log("searching for:", userQuery);
-
-  $.get('https://www.reddit.com/search.json', {
-    q: userQuery
-  }).done(function(response) {
-    console.log(response);
-
-    var results = response.data.children;
-    for (var i = 0; i < results.length; i++) {
-    	var result = results[i].data;
-    	addSearchResult(result);
-    }
-  });
+  //TO DO
 }
 
 // Clear previous search results.
 function clearSearchResults() {
-  $("#results").html("");
+  //TO DO
 }
 
 // Adds a single result object to the page.
 function addSearchResult(result) {
+  //TO DO
   // Create a list item to contain the search result link
-  var li = document.createElement("li");
 
   // create an anchor tag
-  var link = document.createElement("a");
-  link.href = result.url; // reset the value of the the href
-  link.textContent = result.title; // set the value of the text in the link
 
   // put the link inside the list item.
-  $(li).append(link);
 
   // add the list item to the list of search results
-  $("#results").append(li);
 }
